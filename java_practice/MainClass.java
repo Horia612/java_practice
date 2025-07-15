adb820fd04be759dab1767ec8d61ad6bf960a84a
Horia-Stefan Spataru <hspataru@yahoo.com>
2025-07-15 22:16:20 +0300

    Add files via upload

---
+++ java_practice/MainClass.java
@@ -0,0 +1,14 @@
+package java_practice;
+
+public class MainClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("hello");
+
+	}
+
+}
