fb3958be0030b3a1705e290fa3ebd7f1b69fc16c
Spataru HoriaStefan <horia.spataru01@e-uvt.ro>
2025-07-17 01:07:44 +0300

    extended user class

--- java_practice/MainClass.java
+++ java_practice/MainClass.java
@@ -6,7 +6,11 @@ public class MainClass
 	{
 		user testEmployee = new user();
 		testEmployee.setFirstName("Jeff");
-		System.out.print(testEmployee.firstName());
+		testEmployee.setLastName("Woods");
+		testEmployee.setAge(23);
+		testEmployee.setScore(9.8);
+		String message = String.format("Employee %s %s of age %d was hired with admission score of %.2f ! ", testEmployee.lastname(), testEmployee.firstName(), testEmployee.age(), testEmployee.score());
+		System.out.print(message);
 	}
 
 }
--- java_practice/user.java
+++ java_practice/user.java
@@ -4,6 +4,15 @@ public class user
 {
 	public String firstName() { return mFirstName; }
 	public void setFirstName(String name) { mFirstName = name; }
+	public String lastname() { return mLastName; }
+	public void setLastName(String name) { mLastName = name; }
+	public int age() { return mAge; }
+	public void setAge(int age) { mAge = age; }
+	public double score() { return mScore; }
+	public void setScore(double score) { mScore = score; }
 	
 	private String mFirstName;
+	private String mLastName;
+	private int mAge;
+	private double mScore;
 }
